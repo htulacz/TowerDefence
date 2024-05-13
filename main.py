060d1d71a0a7ff0188f55740b10c68b05ec4dc9e
Hubert Tułacz <htulacz@student.agh.edu.pl>
2024-05-13 16:59:16 +0200

    leaderboard w menu

--- main.py
+++ main.py
@@ -14,24 +14,42 @@ run = True
 start_image = pg.image.load("assets/menustart.png")
 leaderboard_image = pg.image.load("assets/menuleaderboard.png")
 exit_image = pg.image.load("assets/menuexit.png")
-
+leave_image = pg.image.load("assets/menuleave.png")
 start_button = Button(c.SCREEN_WIDTH//2, c.SCREEN_HEIGTH//3 - 150, start_image)
 leaderboard_button = Button(c.SCREEN_WIDTH//2, c.SCREEN_HEIGTH//3 * 2 - 150, leaderboard_image)
 exit_button = Button(c.SCREEN_WIDTH//2, c.SCREEN_HEIGTH - 150, exit_image)
+leave_button = Button(0, 0, leave_image)
 
+leaderboard_is_open = False
+scoreboard_surface = None
 
-run = True
 while run:
     clock.tick(c.FPS)
 
     screen.fill("grey100")
-
-    if start_button.draw(screen):
+    if not leaderboard_is_open and start_button.draw(screen):
         play()
-    if leaderboard_button.draw(screen):
-        print("todo")
-    if exit_button.draw(screen):
+    if not leaderboard_is_open and leaderboard_button.draw(screen):
+        leaderboard_is_open = True
+        with open("scores/scores.json", "r") as file:
+            scoreboard = json.load(file)
+        scoreboard_surface = pg.Surface((c.SCREEN_WIDTH, c.SCREEN_HEIGTH))
+        scoreboard_surface.fill((255, 255, 255))
+        font = pg.font.Font(None, 36)
+        y = 50
+        for s in scoreboard:
+            text = font.render(str(s), True, (0,0,0))
+            text_rect = text.get_rect()
+            text_rect.center = (c.SCREEN_WIDTH // 2, y)
+            y += 40
+            scoreboard_surface.blit(text, text_rect)
+    if not leaderboard_is_open and exit_button.draw(screen):
         run = False
+
+    if leaderboard_is_open:
+        if leave_button.draw(scoreboard_surface):
+            leaderboard_is_open = False
+        screen.blit(scoreboard_surface, (0, 0))
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
