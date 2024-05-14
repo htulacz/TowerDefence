76d8e838312ffea83523a4cf4a4b24380f9d404a
Weronika Tatarek <weronikatatarek@interia.pl>
2024-05-14 02:27:10 +0200

    commit

--- main.py
+++ main.py
@@ -23,23 +23,32 @@ tower_button_img = pg.image.load("towerbutton.png").convert_alpha()
 cancel_button_img = pg.image.load("cancelbutton.png").convert_alpha()
 map_image = pg.image.load("assets/maps/map1.png").convert_alpha()
 tower_spot_image = pg.image.load("assets/towerspot.png").convert_alpha()
-enemies_images={
-    "weak": pg.image.load("assets/enemies/e1.png").convert_alpha(),
-    "medium": pg.image.load("assets/enemies/e2.jpg").convert_alpha(),
-    "strong": pg.image.load("assets/enemies/e3.png").convert_alpha(),
-    "elite": pg.image.load("assets/enemies/e4.png").convert_alpha()
-}
+# enemies_images={
+#     "weak": pg.image.load("assets/enemies/e1.png").convert_alpha(),
+#     "medium": pg.image.load("assets/enemies/e2.jpg").convert_alpha(),
+#     "strong": pg.image.load("assets/enemies/e3.png").convert_alpha(),
+#     "elite": pg.image.load("assets/enemies/e4.png").convert_alpha()
+# }
+#
+#
+# map_images={
+#         "1" : pg.image.load("assets/maps/map2.jpeg").convert_alpha(),
+#         "2" : pg.image.load("assets/maps/map2.jpeg").convert_alpha(),
+#         "3" : pg.image.load("assets/maps/map3.jpg").convert_alpha(),
+#         "4" : pg.image.load("assets/maps/map4.jpg").convert_alpha(),
+#         "5" : pg.image.load("assets/maps/map5.png").convert_alpha(),
+#         "6" : pg.image.load("assets/maps/map6.png").convert_alpha(),
+#         "7" : pg.image.load("assets/maps/map7.png").convert_alpha(),
+#         "8" : pg.image.load("assets/maps/map8.png").convert_alpha()
+#     }
+# with open('assets/points/points1.tmj') as file:
+#     world_data = json.load(file)
 
-
-
-with open('assets/points/points1.tmj') as file:
-    world_data = json.load(file)
-
-world = World(world_data,map_image)
-world.process_data()
-world.process_enemies()
-
-last_enemy_spawn = pg.time.get_ticks()
+# world = World(world_data,map_images)
+# world.process_data()
+# world.process_enemies()
+level_started=False
+#last_enemy_spawn = pg.time.get_ticks()
 placing_towers = False
 selected_towers = None
 
@@ -55,13 +64,16 @@ leave_button = Button(0, 0, leave_image)
 
 leaderboard_is_open = False
 scoreboard_surface = None
-
+level=1
 while run:
-
+    # world = World(world_data, map_images)
+    # world.level=level
+    # world.process_data()
+    # world.process_enemies()
     clock.tick(c.FPS)
-
     screen.fill("grey100")
     if not leaderboard_is_open and start_button.draw(screen):
+        ############################################################
         play()
     if not leaderboard_is_open and leaderboard_button.draw(screen):
         leaderboard_is_open = True
@@ -90,4 +102,5 @@ while run:
             run = False
 
     pg.display.flip()
+
 pg.quit()
